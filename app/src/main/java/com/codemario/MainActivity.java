f2f1a078162cd559a632ab8f7b83d7f83e01e01f
Mario sanchez <mariojose1693@gmail.com>
2016-06-10 04:09:54 -0600

    [10-06-16] Returnin a result from a activity

--- app/src/main/java/com/codemario/MainActivity.java
+++ app/src/main/java/com/codemario/MainActivity.java
@@ -7,29 +7,50 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private String dataImport = "Default Value";
+
+    public static final String REQUEST_RESULT = "REQUEST_RESULT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /***
+     * Method read a value from EditText and set to TextView
+     * @param v
+     */
+
     public void consoleLog(View v){
         TextView mytext = (TextView) findViewById(R.id.textView);
-        mytext.setText("Hola Mario");
         EditText txtData = (EditText) findViewById(R.id.txtDato);
         this.dataImport = (String)txtData.getText().toString();
+        mytext.setText(this.dataImport);
         Log.d("debugger","Control tengo chance");
     }
 
+    /***
+     * Method send my property class to [OtherActivity]
+     * @param v
+     */
     public void  loadOtherActivity(View v){
         Intent myIntent = new Intent(this,OtherActivity.class);
         myIntent.putExtra(Intent.EXTRA_TEXT,this.dataImport);
 
-        startActivity(myIntent);
+        startActivityForResult(myIntent,1);
+//        startActivity(myIntent);
+    }
+
+    protected void startActivityForResult(int requestCode,int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (resultCode==RESULT_OK){
+            Toast.makeText(this, data.getIntExtra(REQUEST_RESULT,0), Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
--- app/src/main/java/com/codemario/OtherActivity.java
+++ app/src/main/java/com/codemario/OtherActivity.java
@@ -1,7 +1,10 @@
 package com.codemario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class OtherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+//        get info from [MainActivity]
+        TextView lblMessage = (TextView) findViewById(R.id.lblh1_OtherActivity);
+        if (getIntent()!=null && getIntent().hasExtra(Intent.EXTRA_TEXT)){
+            lblMessage.setText(getIntent().getStringExtra(Intent.EXTRA_TEXT));
+        }
+    }
+
+    public void onClickClose(View v){
+        Intent myIntent = new Intent();
+        myIntent.putExtra(MainActivity.REQUEST_RESULT,42);
+        setResult(RESULT_OK,myIntent);
+        finish();
     }
 }
